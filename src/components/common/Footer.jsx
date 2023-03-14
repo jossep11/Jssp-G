4ce5c4e2c2eaeed39247fd61d7728110c13917f6
JossepP <jossepparedes@gmail.com>
2023-03-14 16:11:53 -0400

    app

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import '../../style/index.css'
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        
+        <p>Copyright © JossepP all rights reserved. Powered by <a className="colorhref" href="https://www.importba.com/" target="_blank" rel="noreferrer">JsspDev</a>.</p>
+      </footer>
+
+    </>
+  )
+}
